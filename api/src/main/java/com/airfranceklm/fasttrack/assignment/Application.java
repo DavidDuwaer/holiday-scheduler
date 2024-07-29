a6cbc339a26a7cc9290c61c40f25705e470f8652
David Duwaer <david@duwaer.nl>
2024-07-29 13:20:43 +0200

    perf: remove global entanglement through global bean namespace collissions

--- api/src/main/java/com/airfranceklm/fasttrack/assignment/Application.java
+++ api/src/main/java/com/airfranceklm/fasttrack/assignment/Application.java
@@ -3,8 +3,9 @@ package com.airfranceklm.fasttrack.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 
-@SpringBootApplication
+@SpringBootApplication(nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
